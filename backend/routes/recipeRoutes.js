0692d2707f1cca78264043e187d9f1696a8a5c58
HeetMorker <heet100500@gmail.com>
2024-09-24 05:25:57 +0530

    recipeRoutes changes

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -18,27 +18,6 @@ router.put('/:id', protect,  upload.single('image'),updateRecipe);
 router.delete('/:id', protect, deleteRecipe);
 
 
-// Backend API to search and paginate recipes
-router.get('/recipes', async (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const searchTerm = req.query.searchTerm || '';
-  const pageSize = 6;
-
-  const query = searchTerm
-      ? { title: new RegExp(searchTerm, 'i') } // Case-insensitive search by title
-      : {};
-
-  const recipes = await Recipe.find(query)
-      .skip((page - 1) * pageSize)
-      .limit(pageSize);
-
-  const totalRecipes = await Recipe.countDocuments(query);
-  const totalPages = Math.ceil(totalRecipes / pageSize);
-
-  res.json({ recipes, totalPages });
-});
-
-
 
 
 module.exports = router;
