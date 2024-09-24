5816565260ed1ffb4f2d0ced828f5eda91454f40
HeetMorker <heet100500@gmail.com>
2024-09-24 06:04:21 +0530

    changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,25 @@
 // /config/db.js
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGO_URI);
+//     console.log("DB Connected ");
+//   } catch (error) {
+//     console.error(`Error: ${error.message}`);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
+
+
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("DB Connected ");
-  } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
-  }
+  await mongoose.connect(process.env.MONGO_URI);
+  console.log("Database Connected Successfully");
 };
 
 module.exports = connectDB;
+
