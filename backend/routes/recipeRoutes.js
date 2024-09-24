5e9bde3c3a9c37866c9fd2ea8cfa4c3dbf45314a
HeetMorker <heet100500@gmail.com>
2024-09-24 05:30:50 +0530

    changes in reciperoutes file

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -8,7 +8,6 @@ const {
   deleteRecipe
 } = require('../controllers/recipeController');
 const upload = require('../middleware/uploadMiddleware');
-
 const router = express.Router();
 
 router.post('/', protect, upload.single('image'), createRecipe);
@@ -16,8 +15,5 @@ router.get('/', getRecipes);
 router.get('/:id', getRecipeById);
 router.put('/:id', protect,  upload.single('image'),updateRecipe);
 router.delete('/:id', protect, deleteRecipe);
-
-
-
 
 module.exports = router;
